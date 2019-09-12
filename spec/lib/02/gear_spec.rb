c27e68dfe13f9bdf0a137d7080aa2f425a0afff6
Manu Phatak <manu.phatak@procore.com>
2019-09-11 20:41:54 -0700

    rubocop

--- spec/lib/02/gear_spec.rb
+++ spec/lib/02/gear_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require '02/gear.rb'
 
 RSpec.describe Gear do
-  it "finds  the ratio" do
+  it 'finds  the ratio' do
     expect(Gear.new(52, 11).ratio).to eq(4.7272727272727275)
   end
-  it "finds  the ratio" do
+  it 'finds  the ratio' do
     expect(Gear.new(30, 27).ratio).to eq(1.1111111111111112)
   end
 end
