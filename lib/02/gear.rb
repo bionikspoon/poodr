ae952e6e0101bd3dd1f887209dd158e70cd4f23b
Manu Phatak <manu.phatak@procore.com>
2019-09-11 20:37:56 -0700

    setup gear class

---
+++ lib/02/gear.rb
@@ -0,0 +1,14 @@
+class Gear
+  def initialize(chainring, cog)
+    @chainring = chainring
+    @cog = cog
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  private
+
+  attr_reader :chainring, :cog
+end
---
+++ spec/lib/02/gear_spec.rb
@@ -0,0 +1,10 @@
+require '02/gear.rb'
+
+RSpec.describe Gear do
+  it "finds  the ratio" do
+    expect(Gear.new(52, 11).ratio).to eq(4.7272727272727275)
+  end
+  it "finds  the ratio" do
+    expect(Gear.new(30, 27).ratio).to eq(1.1111111111111112)
+  end
+end
