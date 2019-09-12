eec5d99f131fc2af1023c5068a33975026cae75c
Manu Phatak <manu.phatak@procore.com>
2019-09-11 21:18:21 -0700

    add gear_inches

--- lib/02/gear.rb
+++ lib/02/gear.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
 class Gear
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
 
   def ratio
     chainring / cog.to_f
   end
 
+  def gear_inches
+    ratio * (rim + (tire * 2))
+  end
+
   private
 
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 end
--- spec/lib/02/gear_spec.rb
+++ spec/lib/02/gear_spec.rb
@@ -3,10 +3,21 @@
 require '02/gear.rb'
 
 RSpec.describe Gear do
-  it 'finds  the ratio' do
-    expect(Gear.new(52, 11).ratio).to eq(4.7272727272727275)
+  describe '#ratio' do
+    it 'finds  the ratio' do
+      aggregate_failures do
+        expect(Gear.new(52, 11, 26, 1.5).ratio).to eq(4.7272727272727275)
+        expect(Gear.new(30, 27, 26, 1.5).ratio).to eq(1.1111111111111112)
+      end
+    end
   end
-  it 'finds  the ratio' do
-    expect(Gear.new(30, 27).ratio).to eq(1.1111111111111112)
+
+  describe '#gear_inches' do
+    it 'shows the effect of whel size on distance traveled' do
+      aggregate_failures do
+        expect(Gear.new(52, 11, 26, 1.5).gear_inches).to eq(137.0909090909091)
+        expect(Gear.new(52, 11, 24, 1.25).gear_inches).to eq(125.27272727272728)
+      end
+    end
   end
 end
