4fbc5a61dfde6e7ceca24e5613723613b51e95e8
Manu Phatak <manu.phatak@procore.com>
2019-09-14 12:45:42 -0700

    chapter 07

--- lib/chapter_07/bicycle.rb
+++ lib/chapter_07/bicycle.rb
@@ -8,26 +8,33 @@ module Chapter07
       @size = opts[:size]
       @chain = opts[:chain] || default_chain
       @tire_size = opts[:chain] || default_tire_size
-    end
 
-    def default_tire_size
-      raise NotImplementedError, "#{self.class} must implement 'default_tire_size'"
+      post_initialize(**opts)
     end
 
     def default_chain
       '11-speed'
     end
 
+    def default_tire_size
+      raise NotImplementedError, "#{self.class} must implement 'default_tire_size'"
+    end
+
+    def post_initialize(**opts); end
+
     def spares
-      { chain: chain, tire_size: tire_size }
+      { chain: chain, tire_size: tire_size }.merge(local_spares)
+    end
+
+    def local_spares
+      {}
     end
   end
 
   class RoadBike < Bicycle
     attr_reader  :tape_color
 
-    def initialize(**opts)
-      super
+    def post_initialize(**opts)
       @tape_color = opts[:tape_color]
     end
 
@@ -35,15 +42,15 @@ module Chapter07
       '23'
     end
 
-    def spares
-      super.merge(tape_color: tape_color)
+    def local_spares
+      { tape_color: tape_color }
     end
   end
+
   class MountainBike < Bicycle
     attr_reader :front_shock
 
-    def initialize(**opts)
-      super
+    def post_initialize(**opts)
       @front_shock = opts[:front_shock]
     end
 
@@ -51,8 +58,28 @@ module Chapter07
       '2.1'
     end
 
-    def spares
-      super.merge(tire_size: tire_size, front_shock: front_shock)
+    def local_spares
+      { front_shock: front_shock }
+    end
+  end
+
+  class RecumbentBike < Bicycle
+    attr_reader :flag
+
+    def post_initialize(**opts)
+      @flag = opts[:flag]
+    end
+
+    def default_chain
+      '10-speed'
+    end
+
+    def default_tire_size
+      '28'
+    end
+
+    def local_spares
+      { flag: flag }
     end
   end
 end
--- spec/lib/chapter_07/bicycle_spec.rb
+++ spec/lib/chapter_07/bicycle_spec.rb
@@ -28,4 +28,16 @@ describe Bicycle do
       expect(bike).to have_attributes(spares: { chain: '11-speed', tire_size: '2.1', front_shock: 'Manitou' })
     end
   end
+
+  describe RecumbentBike do
+    subject(:bike) { RecumbentBike.new(size: 'L', flag: 'tall and orange') }
+
+    it 'has a size' do
+      expect(bike).to have_attributes(size: 'L')
+    end
+
+    it "knows it's spares" do
+      expect(bike).to have_attributes(spares: { chain: '10-speed', tire_size: '28', flag: 'tall and orange' })
+    end
+  end
 end
